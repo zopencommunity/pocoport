fccb50ced7126278c7cd64bfb770c49903833718
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:22:34 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/pocoport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/pocoport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/pocoport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection of C++ class libraries, conceptually similar to the Java Class Library or the .NET Framework.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/pocoport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection of C++ class libraries, conceptually similar to the Java Class Library or the .NET Framework.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
